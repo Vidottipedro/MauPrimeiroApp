95a36c17b5907db1cedcdd5128de1e25a1cf0fae
Usuario <Usuario@CPD03-M19>
2024-02-19 15:07:50 -0300

    Adicionar arquivos de projeto.

---
+++ MauPrimeiroApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauPrimeiroApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
